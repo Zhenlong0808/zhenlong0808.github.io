cade8570f31cc50da8968745f048e3bf32bd55e8
Zhenlong0808 <145696222+Zhenlong0808@users.noreply.github.com>
2026-08-18 00:09:59 +0000

    Deploying to gh-pages from @ Zhenlong0808/zhenlong0808.github.io@d973db0ccdcdf6a1001d8679820629609c91a8a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,14 +423,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%7A%68%65%6E%6C%6F%6E%67%30%38%30%38@%62%65%72%6B%65%6C%65%79.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -444,7 +437,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=MJGS39wAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
